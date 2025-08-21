8cf1b054d2fa8ae9988e93dffa5e6fff84e0fdd1
BhaveshSidhwani <sidhwanibhavesh@gmail.com>
2025-08-20 21:32:57 -0700

    [Chore] Warning message for contact form under development

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,6 +5,15 @@ export default function ContactPage() {
   return (
     <div className="space-y-4">
       <SectionHeader title="Contact" />
+      <p className="text-[--error]">
+        Contact form is currently under development. If you need to reach me,
+        please email{" "}
+        <a className="text-accent-500" href="mailto:sidhwanibhavesh@gmail.com">
+          sidhwanibhavesh@gmail.com
+        </a>
+        .
+        <br />
+      </p>
       <ContactForm />
     </div>
   );
